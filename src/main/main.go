702216c42754904828891821f99440c8fb54ec4d
cifffen <christianfsaetre@gmail.com>
2014-02-07 17:30:41 +0100

    07.02
    Added orderhandler and updated main and fsm

--- src/main/main.go
+++ src/main/main.go
@@ -3,16 +3,28 @@ package main
 import (
 	"../drivers"
 	"../fsm"
+	"../orderHandler"
 )
 func main() int{
 	if fsm.InitElev()==0{
 		fmt.Printf("Unable to initialize elevator hardware.\n")
 		return 1
 	}
-	for {
-		drivers.EventHandler()
+	orderReachedEvent := make(chan bool)
+	timerOutEvent := make(chan bool)
+	newOrderEvent:= make (chan bool)
+	go AtOrder(orderReachedEvent)
+	go GetOrder(NewOrderEvent)
+	for{
+		select{
+			case <-orderReachedEvent:
+				stateMachine(OrderReached)
+			case <- timerOutEvent:
+				statemachine(TimerFinished)
+			case <- NewOrderEvent:
+				statemachine(NewOrder)
+		}
 	}
-	
 	
 	return 0
 }
