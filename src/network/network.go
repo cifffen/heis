afab7e14023a96d0211a3b4d6a7817dfa877eafe
cifffen <christianfsaetre@gmail.com>
2014-03-03 14:19:00 +0100

    Updates 3.3

--- src/network/network.go
+++ src/network/network.go
@@ -4,11 +4,12 @@ import(
 	"net"
 	"fmt"
 	"encoding/json"
+	"log"
 )
 
 var sock *net.UDPConn
 type ActionType int
-const NetworkIp = "192.168.1.255"
+const BroadCastIp = "192.168.1.255"
 const NetworkPort = ":2224"
 
 const(
@@ -33,47 +34,32 @@ func BroadcastOnNet(msg ButtonMessage)(){
 	//rAddr, _ := net.ResolveUDPAddr("udp", "192.168.1.255:2224")
 	_,err1 =sock.WriteTo(buf,addr)
 	if err1 != nil{
-		fmt.Printf("1\n")
-		fmt.Println(err1)
-		fmt.Printf("1\n")
+		log.Println(err1)
 	}
 }
-//func listenOnNetwork()(){
-func main()(){
-	var m ButtonMessage
-	addr, _ := net.ResolveUDPAddr("udp", NetworkPort)
-	sock, _ = net.ListenUDP("udp", addr)
-	fmt.Println("Connected")
-	m1 :=ButtonMessage{
-		Floor: 5,
-		Button: 2,
-		Action: Tender,
+func listenOnNetwork()(){
+	addr, err := net.ResolveUDPAddr("udp", BroadCastPort)
+	if err != nil {
+		log.Printf("Error: %v. Runing without network connetion", err)
+		return
 	}
-	BroadcastOnNet(m1)
-	i:=0
-	for {
-		if i >=10{
-			break
-		}
-		i++
-		buf := make([]byte, 1024)
-		rlen, address, err := sock.ReadFromUDP(buf)
-		sAddr, _ := net.ResolveUDPAddr("udp", "localhost:2224")
-		if address != sAddr{
-			err = json.Unmarshal(buf[0:rlen], &m)
-			fmt.Println(address)
-			BroadcastOnNet(m)
-			if err != nil {
-			  fmt.Println(err)
+	sock, err = net.ListenUDP("udp", addr)
+	if err != nil {
+		log.Printf("Error: %v. Runing without network connetion", err)
+		return
+	}
+	fmt.Println("Listnening on port", addr)
+	go func(){
+		for {
+			buf := make([]byte, 1024)
+			rlen, addr, err := sock.ReadFromUDP(buf)
+			sAddr, _ := net.ResolveUDPAddr("udp", "localhost:2224")
+			if addr != sAddr{
+				err = json.Unmarshal(buf[0:rlen], &m)
+				if err != nil {
+				  log.Printf("Error: %v. Sending aborted", err)
+				}
 			}
-			fmt.Println(buf[0:rlen])
-			//send(m)
-			//_, err = sock.WriteToUDP(buf,address)
-			if err != nil{
-				fmt.Println(err)
-			}
-			fmt.Printf("Floor: %d, Button: %d\n, Action: %d", m.Floor, m.Button, m.Action)
-			//go handlePacket(buf, rlen)
 		}
 	}
 }
