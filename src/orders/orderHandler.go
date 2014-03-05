72f649d540387d87c21d01fabfa0224d63a6c611
cifffen <christianfsaetre@gmail.com>
2014-03-05 10:41:42 +0100

    Div
    Satt inn range, kjørt gofmt etc

--- src/orders/orderHandler.go
+++ src/orders/orderHandler.go
@@ -86,8 +86,8 @@ func OrderHandler() {
 // Check for orders
 func getOrders() bool {
 	firstOrderEvent := false
-	for i := 0; i < Floors; i++ {
-		for j := 0; j < 3; j++ {
+	for i := range OrderMatrix {
+		for j := range OrderMatrix[i] {
 			if (i != 0 && i != Floors-1) || (i == 0 && j != 1) || (i == Floors-1 && j != 0) { // Statement that makes sure that we don't check the Down button at the groud floor and
 				if drivers.ElevGetButtonSignal(j, i) == 1 && orderMatrix[i][j] != 1 { // the Up button at the top floor, as they don't exist.
 					orderMatrix[i][j] = 1
@@ -134,19 +134,20 @@ func atOrder() bool {
 		prevFloor = floor
 		drivers.ElevSetFloorIndicator(floor) //Set floor indicator
 		if floor == Floors-1 {               // If the elevator is at the top floor the direction is changed as it can't go further Upwards.
-			direction = Down
+			//direction = Down
 			atEndFloor = true
 		} else if floor == 0 { // If the elevator is at the bottom floor the direction is changed as it can't go further Downwards.
-			direction = Up
+			//direction = Up
 			atEndFloor = true
 		} else {
 			atEndFloor = false
 		}
+		dir:= ReturnDirection()
 		if orderMatrix[floor][2] == 1 || firstOrderFloor == floor { // Stop if an order from the inside panel has been made at the current floor.
 			firstOrderFloor = -1
 			DeleteFloorOrders(floor)
 			return true
-		} else if (direction == Up && orderMatrix[floor][0] == 1) || (direction == Down && orderMatrix[floor][1] == 1) { // Stop if an order from the direction button at the current floor has been made and the elevator is going in that direction.
+		} else if (dir == Up && orderMatrix[floor][0] == 1) || (dir == Down && orderMatrix[floor][1] == 1) { // Stop if an order from the direction button at the current floor has been made and the elevator is going in that direction.
 			DeleteFloorOrders(floor)
 			return true
 		}
@@ -165,8 +166,8 @@ func GetDir() Direction {
 	var ordersInDir [2]bool // [0] is true if there are orders further up, [1] is true if there is any up
 	var currDir int         // Varable to hold the current direction to be used in orderInDir. 0 for up and 1 for down.
 
-	for i := 0; i < Floors; i++ {
-		for j := 0; j < 3; j++ {
+	for i := range orderMatrix {
+		for j := range orderMatrix[i]{
 			if orderMatrix[i][j] == 1 {
 				if i == prevFloor { //check for orders at current floor
 					ordersAtCur[j] = true
